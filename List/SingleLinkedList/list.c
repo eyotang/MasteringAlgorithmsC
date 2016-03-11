510d82accaee56b31b863c8808dd9cf834fa08cc
Ben Tang <ben.tang@ericsson.com>
2016-03-12 07:12:28 +0800

    Draft version of list

---
+++ List/SingleLinkedList/list.c
@@ -0,0 +1,110 @@
+/* list.c */
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "list.h"
+
+
+void list_init(List *list, void (*destroy)(void *data))
+{
+    /* Initialize the list */
+    list->size = 0;
+    list->head = NULL;
+    list->tail = NULL;
+
+    list->destroy = destroy;
+
+    return;
+}
+
+void list_destroy(List *list)
+{
+    void *date = NULL;
+    /* Remove each element */
+    while (list_size(list) > 0)
+    {
+        if ((list_rm_next(list, NULL, (void **)&date) == 0) && (NULL != list->destroy))
+        {
+            list->destroy(data);
+        }
+    }
+
+    memset(list, 0, sizeof(List));
+    return;
+}
+
+int list_ins_next(List *list, ListElmt *element, const void *data)
+{
+    ListElmt *new_element = NULL;
+    new_element = (ListElmt *)malloc(sizeof(ListElmt));
+    if (NULL == new_element)
+    {
+        return -1;
+    }
+    new_element->data = (void*)data;
+
+    if (NULL == element)
+    {
+        if (0 == list_size(list))
+        {
+            list->tail = new_element;
+        }
+        new_element->next = list->head;
+        list->head = new_element;
+    }
+    else
+    {
+        if (NULL == element->next)
+        {
+            list->tail = new_element;
+        }
+        new_element->next = element->next;
+        element->next = new_element;
+    }
+
+    list->size++;
+    return 0;
+}
+
+int list_rm_next(List *list, ListElmt *element, void **data)
+{
+    ListElmt *old_element = NULL;
+    if (0 == list_size(list))
+    {
+        return -1;
+    }
+
+    if (NULL == element)
+    {
+        *data = list->head->data;
+        old_element = list->head;
+        list->head = list->head->next;
+
+        if (list_size(list) == 1)
+        {
+            list->tail = NULL;
+        }
+    }
+    else
+    {
+        if (NULL == element->next)
+        {
+            return -1;
+        }
+
+        *data = element->next->data;
+        old_element = element->next;
+        element->next = element->next->next;
+
+        if (NULL == element->next)
+        {
+            list->tail = element;
+        }
+    }
+
+    free(old_element);
+    list->size--;
+
+    return 0;
+}
---
+++ List/SingleLinkedList/list.h
@@ -0,0 +1,43 @@
+/* list.h */
+
+#ifndef LIST_H
+#define LIST_H
+
+#include <stdlib.h>
+
+/* Define a structure for linked list elements. */
+typedef struct ListElmt_
+{
+    void *data;
+    struct ListElmt_ *next;
+} ListElmt;
+
+
+/* Define a structrue for linked lists. */
+typedef struct List_
+{
+    int  (*match)(const void *key1, const void *key2);
+    void (*destroy)(void *data);
+
+    int size;
+    ListElmt *head;
+    ListElmt *tail;
+} List;
+
+/* Public interfaces */
+void list_init(List *list, void (*destroy)(void *data));
+void list_destroy(List *list);
+int list_ins_next(List *list, ListElmt *element, const void *data);
+int list_rm_next(List *list, ListElmt *element, void **data);
+
+#define list_size(list) ((list)->size)
+#define list_head(list) ((list)->head)
+#define list_tail(list) ((list)->tail)
+#define list_is_head(list, element) ((element) == (list)->head ? true : false)
+#define list_is_tail(list, element) ((element) == (list)->tail ? true : false)
+#define list_is_empty(list) (((list)->size == 0) ? true : false)
+#define list_data(element) ((element)->data)
+#define list_next(element) ((element)->next)
+
+
+#endif // LIST_H
